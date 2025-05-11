8292e8e5003bccbf3eaf38afa9c65fbef49d7ba1
Muhammad Abid <abidroid1@gmail.com>
2025-05-11 16:47:36 +0500

    structure

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+  // Add welcome screen code here.
+  return <View>
+
+    <Image
+
+    />
+
+    <Text>
+      Little Lemon, your local Mediterranean Bistro
+    </Text>
+
+    <Text>
+      NewsLetter
+    </Text>
+  </View>;
+};
+
+export default WelcomeScreen;
